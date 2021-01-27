ea973bfbb8ebf958e65ddd870a35df5a6af8ae4c
zhuqingguang03 <zhuqingguang03@kuaishou.com>
2021-01-27 21:48:01 +0800

    feat: update

--- src/element-ui/config.ts
+++ src/element-ui/config.ts
@@ -2,11 +2,11 @@ export const AUTO_REGISTER_COMPONENT_COMMAND = 'fast-coding.element.autoRegister
 
 export const ELEMENT_COMPONENTS = [
     "Row", "Col", "Container", "Main", "Header", "Footer", "Grid", 'GridItem',
-    "Button", "Link", "Radio", "Checkbox", "Input", "InputNumber", "Select", "Cascader", 
+    "Button", "Link", "Radio", "Checkbox", "Input", "InputNumber", "Select", "Option", "OptionGroup", "Cascader", 
     "MultiCascader", "Switch", "Slider", "TimePicker", "DatePicker", "AwesomeDatePicker",
-    "Upload", "Rate", "ColorPicker", "Transfer", "Form", "Table", "Tag", "Progress", "Tree", 
+    "Upload", "Rate", "ColorPicker", "Transfer", "Form", "FormItem", "Table", "Tag", "Progress", "Tree", 
     "TreeSelect", "Pagination", "Badge", "Avatar", "Alert", "Loading", "Message", "MessageBox", 
-    "Notification", "NavMenu", "Tabs", "Breadcrumb", "PageHeader", "Dropdown", "Steps", "Dialog", 
+    "Notification", "NavMenu", "Tabs", "TabPane", "Breadcrumb", "PageHeader", "Dropdown", "Steps", "Dialog", 
     "Tooltip", "Popover", "Card", "Carousel", "Collapse", "Timeline", "Divider", "Calendar", 
     "Image", "Backtop", "InfiniteScroll", "Drawer"
 ];
